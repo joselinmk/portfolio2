e26bb8cb79cb6230c36ff1671d055b4468a0414e
joselinmk <mkjoselin2@gmail.com>
2026-05-03 09:52:30 +0530

    pfix error

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,40 +11,35 @@ export const Banner = () => {
   const [text, setText] = useState("");
   const [delta, setDelta] = useState(300 - Math.random() * 100);
 
-  const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
-  const period = 2000;
-
-  const tick = () => {
-    let i = loopNum % toRotate.length;
-    let fullText = toRotate[i];
-    let updatedText = isDeleting
-      ? fullText.substring(0, text.length - 1)
-      : fullText.substring(0, text.length + 1);
-
-    setText(updatedText);
-
-    if (isDeleting) {
-      setDelta((prev) => prev / 2);
-    }
-
-    if (!isDeleting && updatedText === fullText) {
-      setIsDeleting(true);
-      setDelta(period);
-    } else if (isDeleting && updatedText === "") {
-      setIsDeleting(false);
-      setLoopNum((prev) => prev + 1);
-      setDelta(500);
-    }
-  };
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
-    let ticker = setInterval(() => {
-      tick();
+    const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+    const period = 2000;
+
+    const ticker = setInterval(() => {
+      const i = loopNum % toRotate.length;
+      const fullText = toRotate[i];
+      const updatedText = isDeleting
+        ? fullText.substring(0, text.length - 1)
+        : fullText.substring(0, text.length + 1);
+
+      setText(updatedText);
+
+      if (isDeleting) {
+        setDelta((prev) => prev / 2);
+      }
+
+      if (!isDeleting && updatedText === fullText) {
+        setIsDeleting(true);
+        setDelta(period);
+      } else if (isDeleting && updatedText === "") {
+        setIsDeleting(false);
+        setLoopNum((prev) => prev + 1);
+        setDelta(500);
+      }
     }, delta);
 
     return () => clearInterval(ticker);
-  }, [text, delta]);
+  }, [delta, isDeleting, loopNum, text]);
 
   return (
     <section className="banner" id="home">
@@ -83,7 +78,7 @@ export const Banner = () => {
                     isVisible ? "animate__animated animate__zoomIn" : ""
                   }
                 >
-                  <img src={headerImg} alt="Header Image" />
+                  <img src={headerImg} alt="header" />
                 </div>
               )}
             </TrackVisibility>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,62 +1,93 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import { HashLink } from 'react-router-hash-link';
-import { BrowserRouter as Router} from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 
 export const NavBar = () => {
 
-  const [activeLink, setActiveLink] = useState('home');
+  const [activeLink, setActiveLink] = useState("home");
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
     const onScroll = () => {
-      if (window.scrollY > 50) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    }
+      setScrolled(window.scrollY > 50);
+    };
 
     window.addEventListener("scroll", onScroll);
-
     return () => window.removeEventListener("scroll", onScroll);
-  }, [])
+  }, []);
 
   const onUpdateActiveLink = (value) => {
     setActiveLink(value);
-  }
+  };
 
   return (
-    <Router>
-      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
-        <Container>
-          <Navbar.Brand href="/">
-            
-            <h1 className="imge">JOSELIN</h1>
+    <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+      <Container>
 
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav">
-            <span className="navbar-toggler-icon"></span>
-          </Navbar.Toggle>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
-            </Nav>
-            <span className="navbar-text">
-              <div className="social-icon">
-                <a href="https://www.linkedin.com/in/joselin-m-k-031b11379/"><i className="bi bi-linkedin text-white"></i></a>
-                <a href="https://github.com/joselinmk"><i className="bi bi-github text-white"></i></a>
-                <a href="#"><i class="bi bi-facebook text-white"></i></a>
-              </div>
-              <HashLink to='#connect'>
-                <button className="vvd"><span>Let’s Connect</span></button>
-              </HashLink>
-            </span>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </Router>
-  )
-}
+        <Navbar.Brand href="/">
+          <h1 className="imge">JOSELIN</h1>
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+
+            <Nav.Link
+              href="#home"
+              className={activeLink === "home" ? "active navbar-link" : "navbar-link"}
+              onClick={() => onUpdateActiveLink("home")}
+            >
+              Home
+            </Nav.Link>
+
+            <Nav.Link
+              href="#skills"
+              className={activeLink === "skills" ? "active navbar-link" : "navbar-link"}
+              onClick={() => onUpdateActiveLink("skills")}
+            >
+              Skills
+            </Nav.Link>
+
+            <Nav.Link
+              href="#projects"
+              className={activeLink === "projects" ? "active navbar-link" : "navbar-link"}
+              onClick={() => onUpdateActiveLink("projects")}
+            >
+              Projects
+            </Nav.Link>
+
+          </Nav>
+
+          <span className="navbar-text">
+            <div className="social-icon">
+
+              <a href="https://www.linkedin.com/in/joselin-m-k-031b11379/" target="_blank" rel="noreferrer">
+                <i className="bi bi-linkedin text-white"></i>
+              </a>
+
+              <a href="https://github.com/joselinmk" target="_blank" rel="noreferrer">
+                <i className="bi bi-github text-white"></i>
+              </a>
+
+              {/* FIXED HERE 👇 */}
+              <a href="/" >
+                <i className="bi bi-facebook text-white"></i>
+              </a>
+
+            </div>
+
+            <HashLink to="#connect">
+              <button className="vvd">
+                <span>Let’s Connect</span>
+              </button>
+            </HashLink>
+          </span>
+
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
